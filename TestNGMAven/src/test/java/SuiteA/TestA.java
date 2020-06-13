15d09e65fc7ec077c5b3317fc4659572635f3887
Dhanabal-art <dhanakiri1016@gmail.com>
2020-06-13 12:47:34 +0530

    new

---
+++ TestNGMAven/src/test/java/SuiteA/TestA.java
@@ -0,0 +1,12 @@
+package SuiteA;
+
+import org.testng.annotations.Test;
+
+public class TestA {
+	@Test
+	public void TestB()
+	{
+		System.out.println("TestB sis staring for SuiteA from Test B");
+	}
+
+}
---
+++ TestNGMAven/src/test/java/SuiteB/SuiteBTEst.java
@@ -0,0 +1,12 @@
+package SuiteB;
+
+import org.testng.annotations.Test;
+
+public class SuiteBTEst {
+	@Test
+	public void searchSuiteB()
+	{
+		System.out.println("Strating Test for searchSuiteB");
+	}
+
+}
